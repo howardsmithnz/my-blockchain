ceb9fd80389c8dbb616507235c7c259c188e6d1b
howardsmithnz <howardsmithnz@users.noreply.com>
2017-12-28 13:01:55 +1300

    add Block and Blockchain classes

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block{
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
     }
 
     calculateHash(){
-
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "01/01/2017", "Genesis block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let kadaCoin = new Blockchain();
+kadaCoin.addBlock(new Block(1,"28/12/2017", { amount: 4 }));
+kadaCoin.addBlock(new Block(1,"29/12/2017", { amount: 10 }));
+
+console.log(JSON.stringify(kadaCoin, null, 4));
