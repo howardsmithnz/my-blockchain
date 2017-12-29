04cc8aefdfc8ab2a19d9e711a720070abf5c5671
howardsmithnz <howardsmithnz@users.noreply.com>
2017-12-30 08:25:51 +1300

    Add isChainValid method to main.js

--- main.js
+++ main.js
@@ -32,10 +32,29 @@ class Blockchain{
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
 
 let kadaCoin = new Blockchain();
 kadaCoin.addBlock(new Block(1,"28/12/2017", { amount: 4 }));
 kadaCoin.addBlock(new Block(2,"29/12/2017", { amount: 10 }));
 
-console.log(JSON.stringify(kadaCoin, null, 4));
+console.log("Is blockchain valid?", kadaCoin.isChainValid());
+
+//console.log(JSON.stringify(kadaCoin, null, 4));
